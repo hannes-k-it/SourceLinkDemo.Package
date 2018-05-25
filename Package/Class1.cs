1cb0fd1458d53dcbdefbee82d1f041b2f036f905
Hannes Karask <hannes.karask@gmail.com>
2018-05-25 16:19:35 +1000

    Initial commit

---
+++ Package/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Package
+{
+    /// <summary>
+    /// It's a class
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// An useful function
+        /// </summary>
+        public void Foo()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
